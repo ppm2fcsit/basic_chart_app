eac3aa43031b239efa4e69569be4418a6a3a3579
Pattana Somroob <40190081+pattanps@users.noreply.github.com>
2022-06-28 14:31:17 +0700

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -1,3 +1,9 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
 def mychartapp(SB):
   # -*- coding: utf-8 -*-
 
@@ -84,7 +90,41 @@ def mychartapp(SB):
                       high=df['High'],
                       low=df['Low'],
                       close=df['Close']))
-  fig.show()
   
+  # Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+## add new app
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
   
   
